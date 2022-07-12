4e12015690654bcc343e3c02e83d02809b371412
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-07-13 06:28:45 +0700

    :tractor: re-organize folder

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_picker/src/screen/home_screen.dart';
+import 'package:flutter_picker/src/module/home/screen/home_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
