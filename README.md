f4a03dde84d06c74e3043d1d6935bffde8f976b4
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-07-13 20:53:29 +0700

    :pencil: check & request permission

--- README.md
+++ README.md
@@ -1,16 +1,7 @@
-# flutter_picker
+## Flutter Picker & Permission ##
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+#### List Library ####
+- [Flutter Bloc](https://pub.dev/packages/flutter_bloc)
+- [Equatable](https://pub.dev/packages/equatable)
+- [Image Picker](https://pub.dev/packages/image_picker)
+- [Permission Handler](https://pub.dev/packages/permission_handler)
--- lib/src/module/gallery/bloc/take_gallery_bloc.dart
+++ lib/src/module/gallery/bloc/take_gallery_bloc.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_picker/src/module/gallery/event/take_gallery_event.dart';
 import 'package:flutter_picker/src/module/gallery/state/take_gallery_state.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class TakeGalleryBloc extends Bloc<TakeGalleryEvent, TakeGalleryState> {
   TakeGalleryBloc() : super(const TakeGalleryState()) {
@@ -10,7 +14,24 @@ class TakeGalleryBloc extends Bloc<TakeGalleryEvent, TakeGalleryState> {
   void _openGallery(
     TakeGalleryOpenEvent event,
     Emitter<TakeGalleryState> emit,
-  ) {
-    print('TakeGalleryBloc # open gallery');
+  ) async {
+    if (Platform.isAndroid) {
+      final status = await Permission.storage.status;
+      if (status == PermissionStatus.granted) {
+        _pickFromGallery();
+      } else {
+        final request = await Permission.storage.request();
+        if (request == PermissionStatus.granted) {
+          _pickFromGallery();
+        } else {
+          debugPrint('TakeGalleryBloc # android request permission status DENIED');
+        }
+      }
+    }
   }
+
+  void _pickFromGallery() {
+
+  }
+
 }
