0be0d21e3b5d02bd838e53fe388f0ca62fcd3cca
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-07-14 20:36:55 +0700

    :art: ui take camera

--- lib/src/module/camera/screen/take_camera_screen.dart
+++ lib/src/module/camera/screen/take_camera_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_picker/src/module/camera/bloc/take_camera_bloc.dart';
+import 'package:flutter_picker/src/module/camera/event/take_camera_event.dart';
+import 'package:flutter_picker/src/module/camera/state/take_camera_state.dart';
+import 'package:flutter_picker/src/widgets/my_button.dart';
 
 class TakeCameraScreen extends StatefulWidget {
   static const routeName = 'take_camera';
@@ -27,8 +30,53 @@ class _TakeCameraScreenState extends State<TakeCameraScreen> {
       appBar: AppBar(
         title: const Text('Take Camera'),
       ),
-      body: Center(
-        child: Text('Take Camera'),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildImage(),
+              const SizedBox(height: 16),
+              _buildTextPath(),
+              const SizedBox(height: 16),
+              _buildButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildImage() {
+    return BlocBuilder<TakeCameraBloc, TakeCameraState>(
+      builder: (context, state) {
+        return SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: 200,
+          child: Container(),
+        );
+      },
+    );
+  }
+
+  Widget _buildTextPath() {
+    return BlocBuilder<TakeCameraBloc, TakeCameraState>(
+      buildWhen: (previous, current) => previous.path != current.path,
+      builder: (context, state) {
+        return Text('${state.path}');
+      },
+    );
+  }
+
+  Widget _buildButton() {
+    return Center(
+      child: MyButton(
+        title: 'OPEN CAMERA',
+        onPressed: () {
+          _bloc.add(TakeCameraOpenEvent());
+        },
       ),
     );
   }
