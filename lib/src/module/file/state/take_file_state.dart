a59c1a62b3438f4f1bb64a31955dbe121a32234c
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-07-15 16:37:14 +0700

    :newspaper: add event state take file

---
+++ lib/src/module/file/state/take_file_state.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+class TakeFileState extends Equatable {
+  final String? path;
+
+  const TakeFileState({this.path});
+
+  TakeFileState copyWith({
+    String? path,
+  }) =>
+      TakeFileState(
+        path: path ?? this.path,
+      );
+
+  @override
+  List<Object?> get props => [path];
+}
