eb5da573ac8a40ec6307dfdf66e148fcf98c6eb8
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-01-19 14:22:16 +0700

    :pencil: setup android permission request location

--- lib/src/module/location/bloc/location_bloc.dart
+++ lib/src/module/location/bloc/location_bloc.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_picker/src/module/location/event/location_event.dart';
 import 'package:flutter_picker/src/module/location/state/location_state.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class LocationBloc extends Bloc<LocationEvent, LocationState> {
 
@@ -9,8 +10,21 @@ class LocationBloc extends Bloc<LocationEvent, LocationState> {
     on<GetLocationEvent>(_onGetLocation);
   }
 
-  void _onGetLocation(GetLocationEvent event, Emitter<LocationState> emit) {
+  void _onGetLocation(GetLocationEvent event, Emitter<LocationState> emit) async {
     debugPrint('Location Bloc # get user location');
+    const permission = Permission.location;
+    final permissionStatus = await permission.request();
+    if (permissionStatus == PermissionStatus.granted) {
+
+    } else {
+      final request = await permission.request();
+      if (request == PermissionStatus.granted) {
+
+      } else {
+        emit(state.copyWith(status: null));
+        emit(state.copyWith(status: request));
+      }
+    }
   }
 
 }
