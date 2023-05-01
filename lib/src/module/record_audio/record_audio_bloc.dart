a7026310712d2375391fe1f50eefd3af005112c4
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-05-01 20:27:49 +0700

    :pencil: permission recording android

--- lib/src/module/record_audio/record_audio_bloc.dart
+++ lib/src/module/record_audio/record_audio_bloc.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_picker/src/module/record_audio/record_audio_event.dart';
 import 'package:flutter_picker/src/module/record_audio/record_audio_state.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 
 class RecordAudioBloc extends Bloc<RecordAudioEvent, RecordAudioState> {
   RecordAudioBloc() : super(const RecordAudioState()) {
@@ -21,10 +26,37 @@ class RecordAudioBloc extends Bloc<RecordAudioEvent, RecordAudioState> {
   void _onStart(
     RecordAudioStartEvent event,
     Emitter<RecordAudioState> emit,
-  ) {
-    emit(state.copyWith(
-      isRecording: true,
-    ));
+  ) async {
+    if (Platform.isAndroid) {
+      // for android
+      final deviceInfo = DeviceInfoPlugin();
+      final androidInfo = await deviceInfo.androidInfo;
+      final sdkInt = androidInfo.version.sdkInt;
+
+      if (sdkInt >= 33) {
+        final checkMicStatus = await Permission.microphone.status;
+        debugPrint('RecordAudioBloc # mic status $checkMicStatus');
+        emit(state.copyWith(
+          permissionMicStatus: checkMicStatus,
+        ));
+        if (checkMicStatus == PermissionStatus.granted) {
+          await _startRecording(emit);
+        } else {
+          final request = await Permission.microphone.request();
+          emit(state.copyWith(
+            permissionMicStatus: request,
+          ));
+          if (request == PermissionStatus.granted) {
+            await _startRecording(emit);
+          }
+        }
+      }
+    } else {
+      // for ios
+      emit(state.copyWith(
+        isRecording: true,
+      ));
+    }
   }
 
   void _onStop(
@@ -33,6 +65,14 @@ class RecordAudioBloc extends Bloc<RecordAudioEvent, RecordAudioState> {
   ) {
     emit(state.copyWith(
       isRecording: false,
+    ));
+  }
+
+  Future<void> _startRecording(
+    Emitter<RecordAudioState> emit,
+  ) async {
+    emit(state.copyWith(
+      isRecording: true,
     ));
   }
 }
--- lib/src/module/record_audio/record_audio_state.dart
+++ lib/src/module/record_audio/record_audio_state.dart
@@ -1,22 +1,28 @@
 import 'package:equatable/equatable.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class RecordAudioState extends Equatable {
   final bool isRecording;
+  final PermissionStatus? permissionMicStatus;
 
   const RecordAudioState({
     this.isRecording = false,
+    this.permissionMicStatus,
   });
 
   RecordAudioState copyWith({
     bool? isRecording,
+    PermissionStatus? permissionMicStatus,
   }) {
     return RecordAudioState(
       isRecording: isRecording ?? this.isRecording,
+      permissionMicStatus: permissionMicStatus ?? this.permissionMicStatus,
     );
   }
 
   @override
   List<Object?> get props => [
         isRecording,
+        permissionMicStatus,
       ];
 }
