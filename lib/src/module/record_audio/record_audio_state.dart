3117ca8511cc500ecb3cddfbeb27b3ae7d3071fa
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-05-05 05:23:58 +0700

    :pencil: update state

--- lib/src/module/record_audio/record_audio_state.dart
+++ lib/src/module/record_audio/record_audio_state.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class RecordAudioState extends Equatable {
@@ -6,12 +7,16 @@ class RecordAudioState extends Equatable {
   final PermissionStatus? permissionMicStatus;
   final bool isReadyToPlay;
   final String path;
+  final ProcessingState processingState;
+  final bool isPlaying;
 
   const RecordAudioState({
     this.isRecording = false,
     this.permissionMicStatus,
     this.isReadyToPlay = false,
     this.path = '',
+    this.processingState = ProcessingState.idle,
+    this.isPlaying = false,
   });
 
   RecordAudioState copyWith({
@@ -19,12 +24,16 @@ class RecordAudioState extends Equatable {
     PermissionStatus? permissionMicStatus,
     bool? isReadyToPlay,
     String? path,
+    ProcessingState? processingState,
+    bool? isPlaying,
   }) {
     return RecordAudioState(
       isRecording: isRecording ?? this.isRecording,
       permissionMicStatus: permissionMicStatus ?? this.permissionMicStatus,
       isReadyToPlay: isReadyToPlay ?? this.isReadyToPlay,
       path: path ?? this.path,
+      processingState: processingState ?? this.processingState,
+      isPlaying: isPlaying ?? this.isPlaying,
     );
   }
 
@@ -34,5 +43,7 @@ class RecordAudioState extends Equatable {
         permissionMicStatus,
         isReadyToPlay,
         path,
+        processingState,
+        isPlaying,
       ];
 }
