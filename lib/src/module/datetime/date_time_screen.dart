e0891794135a3346067c7bbca74ad1bb5904a0f2
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-10-19 21:34:55 +0700

    :pencil: show default date picker

--- lib/src/module/datetime/date_time_screen.dart
+++ lib/src/module/datetime/date_time_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_picker/src/widgets/my_button.dart';
 
 class DateTimeScreen extends StatefulWidget {
   static const routeName = 'date_time';
@@ -18,9 +19,37 @@ class _DateTimeScreenState extends State<DateTimeScreen> {
         title: const Text('Date Time'),
       ),
       body: SafeArea(
-        child: Center(
-          child: Text('Date Time Picker'),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _buildDefaultDate(),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildDefaultDate() {
+    return Center(
+      child: MyButton(
+        title: 'DEFAULT DATE PICKER',
+        onPressed: () async {
+          final now = DateTime.now();
+          final lastDate = DateTime(now.year, now.month + 1, now.day);
+          var theDate = await showDatePicker(
+            context: context,
+            initialDate: now,
+            firstDate: now,
+            lastDate: lastDate,
+            initialEntryMode: DatePickerEntryMode.calendar,
+          );
+          if (theDate != null) {
+            debugPrint("DateTimeScreen # ${theDate.toLocal()}");
+          }
+        },
       ),
     );
   }
